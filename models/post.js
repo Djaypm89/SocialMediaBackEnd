6762ba062924338d3964ec7dd9d5374c2f2ee589
Dom B <domlamont@yahoo.com>
2021-12-13 11:28:53 -0800

    Created Post Schema with Validation

---
+++ models/post.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const postSchema = new mongoose.Schema({
+    userId: {type: Number, required: true},
+    postBody: {type: String, required: true},
+    like: {type: Number, default: 0}
+})
+
+const Post = mongoose.model('Post', postSchema);
+
+function validatePost(Post){
+    const schema = Joi.object({
+        userId: Joi.number().required(),
+        postBody: Joi.string().required(),
+        like: Joi.number()
+    });
+    return schema.validate(Post);
+}
+
+function validateLike(Post){
+    const schema = Joi.object({
+        like: Joi.number()
+    })
+    return schema.validate(Post);
+}
+
+module.exports = {
+    Post: Post,
+    validatePost: validatePost,
+    validateLike: validateLike
+}
+
